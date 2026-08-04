f809a4623d312d6b185ee269de31463edd897928
George Mihai Grigore <grigore.george.mihaii@gmail.com>
2026-08-04 10:45:40 +0300

    Clean up SimpleCov config

--- spec/support/simplecov_config.rb
+++ spec/support/simplecov_config.rb
@@ -3,9 +3,6 @@
 require "simplecov"
 
 SimpleCov.start "rails" do
-  add_filter "/bin/"
-  add_filter "/db/"
-  add_filter "/spec/"
-  add_filter "/config/"
-  add_filter "app/admin"
+  skip "/bin/"
+  skip "app/admin"
 end
